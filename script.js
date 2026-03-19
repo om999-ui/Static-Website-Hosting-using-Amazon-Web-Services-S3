5dba49d70ecc0d02e34e1df022c42965154c32ec
om <ommasal1311@gmail.com>
2026-03-20 01:38:49 +0530

    Initial commit - AWS static website

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+    alert("Thanks for visiting my website! 😊");
+}
+
+function showAlert() {
+    alert("Project details coming soon 🚀");
+}
+
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: "smooth"
+    });
+}
